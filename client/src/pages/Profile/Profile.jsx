9b9978403581ac695b942e521ac33aea060bdc82
DeadStreap <74311984+DeadStreap@users.noreply.github.com>
2026-01-27 16:55:02 +0300

    create skelet for profile page

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,1 +1,23 @@
+import { useState, useEffect } from "react";
+import {  Link } from 'react-router-dom';
 
+function Profile() {
+    // const URL = "localhost:3001";
+    // const API = "together-alpha-one.vercel.app";
+    // const API_URL = `https://${API}/api/content/id/${ActivityId}`;
+    const user = {
+        name: 'аххаахах',
+        couple: false,
+
+    }
+    return (
+        <div>
+            <h1>Профиль</h1>
+            <p>Name:{user.name}</p>
+            <p>{user.couple ? `В паре с ${user.couple}` : `У вас пока нет пары`}</p>
+            <Link to='/'>Вернуться на главную </Link>
+        </div>
+    );
+}
+
+export default Profile;
