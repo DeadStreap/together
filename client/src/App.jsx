cbfcdaabb39ad5737dde8fefb1a35b91ec928569
DeadStreap <74311984+DeadStreap@users.noreply.github.com>
2026-01-27 16:55:45 +0300

    add some new route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,13 @@ import { Routes, Route, Link } from 'react-router-dom';
 import AboutPage from './pages/AboutPage';
 import NotFoundPage from './pages/NotFoundPage';
 
+import Profile from './pages/Profile/Profile';
+
 import Activities from './pages/Activity/Activities';
 import ActivityById from './pages/Activity/ActivityById';
+import ActivitiesTogether from './pages/Activity/ActivitiesTogether';
+import ActivitiesAlone from './pages/Activity/ActivitiesAlone';
+
 
 import { Header } from './components/Header'
 
@@ -17,14 +22,15 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Activities />} />
-        <Route path="/profile" element={<Activities />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/authorization" element={<Activities />} />
         <Route path="/register" element={<Activities />} />
 
 
-        <Route path="/together_activity" element={<Activities />} />
         <Route path="/activity/:ActivityId" element={<ActivityById />} />
         <Route path="/activity" element={<Activities />} />
+        <Route path="/activity/alone" element={<ActivitiesAlone />} />
+        <Route path="/activity/together" element={<ActivitiesTogether />} />
 
         <Route path="/activity/create" element={<Activities />} />
 
