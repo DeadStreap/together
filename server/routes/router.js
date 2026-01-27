520118575f80103e2571aa9344ac07816f48d6c1
DeadStreap <74311984+DeadStreap@users.noreply.github.com>
2026-01-27 22:38:29 +0300

    add routes for controllers

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,11 +2,32 @@ const Router = require('express')
 const router = new Router()
 
 const contentController = require('../controller/contentController')
+const userController = require('../controller/userController')
+const coupleController = require('../controller/coupleController')
+const commentController = require('../controller/commentController')
 
 router.get('/content', contentController.getContent)
 router.get('/content/id/:id', contentController.getContentById)
 router.put('/update/content', contentController.updateContentById)
-router.post('/delete/content', contentController.deleteContent)
 router.post('/create/content', contentController.createContent)
+router.post('/delete/content', contentController.deleteContent)
+
+// router.get('/user', userController.getUser)
+// router.get('/user/id', userController.getUserById)
+// router.put('/update/user', userController.updateUser)
+// router.post('/create/user', userController.createUser)
+// router.post('/delete/user', userController.deleteUser)
+
+// router.get('/couple', coupleController.getCouple)
+// router.get('/couple/id', coupleController.getCoupleById)
+// router.put('/update/couple', coupleController.updateCouple)
+// router.get('/create/couple', coupleController.createCouple)
+// router.get('/delete/couple', coupleController.deleteCouple)
+
+// router.get('/comment', commentController.getComment)
+// router.get('/comment/id', commentController.getCommentById)
+// router.put('/update/comment', commentController.updateComment)
+// router.get('/create/comment', commentController.createComment)
+// router.get('/delete/comment', commentController.deleteComment)
 
 module.exports = router;
