679f5ca3b4ed6e9f38c5b0dc170b69bf6f211e44
DeadStreap <74311984+DeadStreap@users.noreply.github.com>
2026-01-27 12:45:07 +0300

    back not env parameters for db connect

--- server/config/db.js
+++ server/config/db.js
@@ -2,14 +2,15 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST || 'bpvc1zsief2yavox1cw7-mysql.services.clever-cloud.com',
+  port: process.env.MYSQL_PORT || 3306,
+  user: process.env.MYSQL_USER || 'ucqa9qdpsstwl4i0',
+  password: process.env.MYSQL_PASSWORD || 'uyeQo6bmhrycuT1jz9KW',
+  database: process.env.MYSQL_DATABASE || 'bpvc1zsief2yavox1cw7',
   connectionLimit: 10,
   queueLimit: 0,
 });
+
 
 async function testConnection() {
   try {
