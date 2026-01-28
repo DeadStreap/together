8ac459deb18e0af4a4a88802361d2919b20fffbe
DeadStreap <74311984+DeadStreap@users.noreply.github.com>
2026-01-28 19:31:15 +0300

    add userProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/reset.css';
 import './index.css';
 import App from './App';
+import { UserProvider } from './store/UserContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </Router>
   </React.StrictMode>
 );
