1e7e709daafae75b1eeac9ff4a6d9344c8636eae
DeadStreap <74311984+DeadStreap@users.noreply.github.com>
2026-01-28 16:59:53 +0300

    refactoring for new design

--- client/src/pages/Activity/ActivitiesAlone.jsx
+++ client/src/pages/Activity/ActivitiesAlone.jsx
@@ -47,53 +47,57 @@ function ActivitiesAlone() {
             {contentItems.length > 0 ? (
                 <ul className="content-list">
                     {contentItems.map((item) => (
-                        <div key={item.id} style={styles.contentItem}>
-                            <Link to={`/activity/${item.id}`} style={{color: '#fff'}}>
-                            <div className="item-title">
-                                <strong>{item.title || "Без названия"}</strong>
-                            </div>
-                            <div className="item-details">
-                                Категория: {item.category || "N/A"}
-                            </div>
-                            <div className="item-details">
-                                Created by: {item.added_by_user_id || "N/A"}
-                            </div>
-                            <div className="item-dates">
-                                <span>
-                                Добавлено:{" "}
-                                    {item.added_at
-                                        ? (() => {
-                                            const date = new Date(item.added_at);
-                                            const formattedDate = date.toLocaleDateString('ru-RU', {
-                                                day: '2-digit',
-                                                month: 'long',
-                                                year: 'numeric'
-                                            });
-                                            const formattedTime = date.toLocaleTimeString('ru-RU', {
-                                                hour: '2-digit',
-                                                minute: '2-digit',
-                                            });
-                                            return `${formattedDate} ${formattedTime}`;
-                                        })()
-                                        : "не указано"}
-                                </span>
-                                <br></br>
-                                <span>
-                                    Начало:{" "}
-                                    {item.start_date
-                                        ? new Date(item.start_date).toLocaleDateString()
-                                        : "не указанно"}
-                                </span>
-                                <br></br>
-                                <span>
-                                    Конец:{" "}
-                                    {item.end_date
-                                        ? new Date(item.end_date).toLocaleDateString()
-                                        : "не указанно"}
-                                </span>
-                            </div>
+                        <li key={item.id} className="content-card">
+                            <Link to={`/activity/${item.id}`} className="content-card-link">
+                                <div className="item-title">
+                                    {item.title || "Без названия"}
+                                </div>
+                                <div className="item-details">
+                                    <span>Категория</span>: {item.category || "N/A"}
+                                </div>
+                                <div className="item-details">
+                                    <span>Создал</span>: {item.added_by_user_id || "N/A"}
+                                </div>
+                                <div className="item-dates">
+                                    <span>
+                                        <span>Добавлено</span>
+                                        <span>
+                                            {item.added_at
+                                                ? (() => {
+                                                    const date = new Date(item.added_at);
+                                                    const formattedDate = date.toLocaleDateString('ru-RU', {
+                                                        day: '2-digit',
+                                                        month: 'long',
+                                                        year: 'numeric'
+                                                    });
+                                                    const formattedTime = date.toLocaleTimeString('ru-RU', {
+                                                        hour: '2-digit',
+                                                        minute: '2-digit',
+                                                    });
+                                                    return `${formattedDate} ${formattedTime}`;
+                                                })()
+                                                : "не указано"}
+                                        </span>
+                                    </span>
+                                    <span>
+                                        <span>Начало</span>
+                                        <span>
+                                            {item.start_date
+                                                ? new Date(item.start_date).toLocaleDateString()
+                                                : "не указано"}
+                                        </span>
+                                    </span>
+                                    <span>
+                                        <span>Конец</span>
+                                        <span>
+                                            {item.end_date
+                                                ? new Date(item.end_date).toLocaleDateString()
+                                                : "не указано"}
+                                        </span>
+                                    </span>
+                                </div>
                             </Link>
-                        </div>
+                        </li>
                     ))}
                 </ul>
             ) : (
