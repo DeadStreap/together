5d1cfd16106d482c3b1d12444098691d3fc1e880
DeadStreap <74311984+DeadStreap@users.noreply.github.com>
2026-01-30 14:25:12 +0300

    add ThemeProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,16 @@ import './styles/reset.css';
 import './index.css';
 import App from './App';
 import { UserProvider } from './store/UserContext';
+import { ThemeProvider } from './store/ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <ThemeProvider>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </ThemeProvider>
     </Router>
   </React.StrictMode>
 );
