645e23d42676fee279d09b93e1582596bb23cfd4
DeadStreap <74311984+DeadStreap@users.noreply.github.com>
2026-02-25 15:04:08 +0300

    back to clever db

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'together.ru',
+  host: process.env.MYSQL_HOST || 'bpvc1zsief2yavox1cw7-mysql.services.clever-cloud.com',
   port: process.env.MYSQL_PORT || 3306,
-  user: process.env.MYSQL_USER || 'u3427285_default',
-  password: process.env.MYSQL_PASSWORD || 'xyGR3SVIo12xIY7K',
-  database: process.env.MYSQL_DATABASE || 'u3427285_default',
+  user: process.env.MYSQL_USER || 'ucqa9qdpsstwl4i0',
+  password: process.env.MYSQL_PASSWORD || 'uyeQo6bmhrycuT1jz9KW',
+  database: process.env.MYSQL_DATABASE || 'bpvc1zsief2yavox1cw7',
   connectionLimit: 10,
   queueLimit: 0,
 });
