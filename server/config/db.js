34a7d0323202bd7696a325fccd48d18040f77315
DeadStreap <gerademakow@gmail.com>
2026-02-28 16:17:53 +0300

    connection fix

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
+  host: process.env.MYSQL_HOST || 'bpvc1zsief2yavox1cw7-mysql.services.clever-cloud.com',
   port: process.env.MYSQL_PORT || 3306,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  user: process.env.MYSQL_USER || 'ucqa9qdpsstwl4i0',
+  password: process.env.MYSQL_PASSWORD || 'uyeQo6bmhrycuT1jz9KW',
+  database: process.env.MYSQL_DATABASE || 'bpvc1zsief2yavox1cw7',
   connectionLimit: 1,
   queueLimit: 10,
   waitForConnections: true,
